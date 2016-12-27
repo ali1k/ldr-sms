0ee0e882752be7ccffc05267485a366243ebb289
ali1k <hyperir@gmail.com>
2016-12-27 17:05:12 +0330

    removed cordis

--- configs/facets.js
+++ configs/facets.js
@@ -201,34 +201,6 @@ export default {
 
             }
         },
-        'http://risis.eu/cordisH2020': {
-            list: [
-                'http://risis.eu/cordisH2020/vocab/fundingScheme',
-                'http://risis.eu/cordisH2020/vocab/topic',
-                'http://risis.eu/cordisH2020/vocab/totalCost',
-                'http://risis.eu/cordisH2020/vocab/callID',
-                'http://rdf-vocabulary.ddialliance.org/discovery#startDate',
-                'http://risis.eu/cordisH2020/vocab/durationMonths'
-            ],
-            config: {
-                'http://www.w3.org/1999/02/22-rdf-syntax-ns#type': {
-                    label: ['Type'],
-                    hint: ['Type of the resource under investigation.']
-                },
-                'http://risis.eu/cordisH2020/vocab/projectParticipant': {
-                    label: ['Participant'],
-                    hasLinkedValue: 1
-                },
-                'http://risis.eu/cordisH2020/vocab/fundingScheme': {
-                    label: ['Funding Scheme'],
-                    hasLinkedValue: 1
-                },
-                'http://risis.eu/cordisH2020/vocab/topic': {
-                    label: ['Topic'],
-                    hasLinkedValue: 1
-                }
-            }
-        },
         'http://www.freme-project.eu/datasets/cordis': {
             list: [
                 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
