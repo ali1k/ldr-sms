1957558a147baa4d19d0dd1ad224448629afce4f
ali1k <hyperir@gmail.com>
2017-01-24 18:22:35 +0100

    resolved geo loading timeout issue

--- actions/getBoundaries.js
+++ actions/getBoundaries.js
@@ -1,5 +1,7 @@
 export default function getBoundaries(context, payload, done) {
-    context.service.read('resource.boundaries', payload, {}, function (err, res) {
+    context.dispatch('LOADING_DATA', {});
+    context.service.read('resource.boundaries', payload, {timeout: 20 * 1000}, function (err, res) {
+        context.dispatch('LOADED_DATA', {});
         if (err) {
             context.dispatch('FIND_BOUNDARIES_FAILURE', err);
         } else {
