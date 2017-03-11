48d4d330d6e7c44664f562301e628facd462a2a6
ali1k <hyperir@gmail.com>
2017-03-11 17:39:18 +0100

    fixed eslint issues

--- demos/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-    googleKey:'AIzaSyDvHC2-4XJIJcgcwRAWywJJ_alaPYFNQCE',
-    //risis key
-    //googleKey:'AIzaSyC803Jo-PnqChR6gTDHm4JrppBdstqWzPI',
-    mapboxKey:'pk.eyJ1IjoicmlzaXMiLCJhIjoiY2llbWhiMzJ1MDAwYnQ5a2t4OTV1NXo0cCJ9.FdMFYriBcsxNErY0wl6PBw'
-}
--- demos/config.sample.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-    googleKey:'put your Google API key here...',
-    mapboxKey: 'put your Mapbox API key here...',
-}
